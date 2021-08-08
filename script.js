ac7259a2958e9022520ace51a0e61edc44f2c6c8
user123 <mo7amedfares321@gmail.com>
2021-08-08 08:29:22 +0200

    asdads

---
+++ script.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const knex = require('knex')({
+  client:"pg",
+  connection:{connectionString:process.env.DATABASE_URL,ssl:true}
+});
+app.get("/",async (req,resp)=>{
+    const data = await knex.select("*").from("users")
+    resp.json(data)
+})
+app.listen(3000)
